303e7b552e1e6e655c0903c895f3f87bac6bed6b
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-10 14:57:12 -0500

    Create 01_helloFunc.py

---
+++ CH 3 Functions/01_helloFunc.py
@@ -0,0 +1,14 @@
+# This is an example of defining a function and calling it.
+# The 'def' keyword tells the interpreter to define the function.
+# hello() is the name of our function.
+# The three 'print' statements compose the body of the function
+# The function is called three times.
+
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+
+hello()
+hello()
+hello()
