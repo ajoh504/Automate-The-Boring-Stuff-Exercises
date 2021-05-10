606829a1759cebe96e817cb67cfc342ff6d05981
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-10 12:41:50 -0500

    Update blank.py

--- CH 1 Python Basics/blank.py
+++ CH 1 Python Basics/blank.py
@@ -1,1 +1,10 @@
+#example of a while Loop
+#the loop repeats infinitely until the user
+#types the exact phrase 'your name'
 
+while True:
+    print ('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you')
