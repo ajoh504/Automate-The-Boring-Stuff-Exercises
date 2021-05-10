01348689e92aad2aec08b541cd00d728b44ed94e
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-10 12:56:14 -0500

    Create littleKid.py

---
+++ CH 2 Flow Control/littleKid.py
@@ -0,0 +1,11 @@
+#an if statement that demonstrates the use of an 'else' block of code
+#the 'else' runs if the first two conditions are false
+
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+else:
+    print('You are neither Alice nor a little kid.')
