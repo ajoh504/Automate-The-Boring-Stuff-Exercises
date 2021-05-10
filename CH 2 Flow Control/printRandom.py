3bf27b5ed0b6becbc39ba241b4d773830f536241
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-10 12:58:19 -0500

    Create printRandom.py

---
+++ CH 2 Flow Control/printRandom.py
@@ -0,0 +1,8 @@
+# Using the import keyward to import the random module.
+# A for loop uses the range() function to print five
+# random numbers  in the range of 1 through 10
+
+import random
+for i in range(5):
+    print(random.randint(1, 10))
+
