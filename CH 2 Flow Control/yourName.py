e4c328a823c583408e23a3434d89635e15309f49
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-10 13:12:41 -0500

    Create yourName.py

---
+++ CH 2 Flow Control/yourName.py
@@ -0,0 +1,7 @@
+#a while loop that is searching for the literal phrase 'your name'
+
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
