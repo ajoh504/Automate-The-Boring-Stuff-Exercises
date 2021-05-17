4472a66662b1d1daef4046ed5789f8af68bdcf61
Alex <83892813+ajoh504@users.noreply.github.com>
2021-05-17 10:39:01 -0500

    Create 01_allMyCats.py

---
+++ CH 4 Lists/01_allMyCats.py
@@ -0,0 +1,22 @@
+# This program stores a group of string variables
+# for each cat name entered. However, the code is
+# problemmatic because it can only receive exactly
+# six cat names. A better way to write the code
+# would be to allow for any number of cat names.
+
+
+print('Enter the name of cat 1:')
+catName1 = input()
+print('Enter the name of cat 2:')
+catName2 = input()
+print('Enter the name of cat 3:')
+catName3 = input()
+print('Enter the name of cat 4:')
+catName4 = input()
+print('Enter the name of cat 5:')
+catName5 = input()
+print('Enter the name of cat 6:')
+catName6 = input()
+print('The cat names are:')
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' +
+catName5 + ' ' + catName6)
