1b9c4231eefa5495ecfbeac3c93e687a116ef81f
Alex <83892813+ajoh504@users.noreply.github.com>
2021-07-26 17:09:04 -0500

    Create 09_commaCode.py

---
+++ CH 4 Lists/09_commaCode.py
@@ -0,0 +1,24 @@
+#receive a list and return a string value
+#with all items separated by commas, and
+#the word "and" before the last item. Must
+#account for an empty list.
+
+def addCommasToList(listToConvert):
+    string = ''
+    if len(listToConvert) == 0:
+        string = 'List contains no items.'
+    elif len(listToConvert) == 1:
+        string = str(listToConvert[0])
+    elif len(listToConvert) == 2:
+        string = str(listToConvert[0]) + ' and ' + str(listToConvert[1])
+    else: 
+        for i in listToConvert :
+            string += str(i) + ', '
+            if i == listToConvert[-2]:
+                string += 'and ' + str(listToConvert[-1])
+                break
+    print(string)
+    return string
+    
+testList = ['salad', 'potato', 'soup']    
+addCommasToList(testList)
