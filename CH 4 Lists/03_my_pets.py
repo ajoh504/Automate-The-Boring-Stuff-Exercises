cf6813c6cbaace55798211a44df2f4deb8383830
Alex <83892813+ajoh504@users.noreply.github.com>
2021-11-01 14:29:18 -0700

    Update and rename 03_myPets.py to 03_my_pets.py
    Change variables and title from camelCase to snake_case

--- CH 4 Lists/03_my_pets.py
+++ CH 4 Lists/03_my_pets.py
@@ -2,10 +2,10 @@
 # to see if an item is stored in it using the
 # 'not in' operator
 
-myPets = ['Zombie','Pooka','Fat-tail']
+my_pets = ['Zombie','Pooka','Fat-tail']
 print('Enter a pet name')
 name = input()
-if name not in myPets:
+if name not in my_pets:
     print('I do not have a pet named ' + name)
 else:
     print(name + ' is my pet.')
