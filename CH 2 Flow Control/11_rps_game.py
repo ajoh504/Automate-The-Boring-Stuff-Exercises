d9b523172de7fd4912ebba03e528e2af249028e9
Alex <83892813+ajoh504@users.noreply.github.com>
2021-11-01 14:14:31 -0700

    Update and rename 11_rpsGame.py to 11_rps_game.py
    Change variables and title from camelCase to snake_case

--- CH 2 Flow Control/11_rps_game.py
+++ CH 2 Flow Control/11_rps_game.py
@@ -14,52 +14,52 @@ while True:
     # the player input loop:
     while True:
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
-        playerMove = input()
-        if playerMove == 'q':
+        player_move = input()
+        if player_move == 'q':
             sys.exit() #Quit the program
-        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+        if player_move == 'r' or player_move == 'p' or player_move == 's':
             break #Break out of the player input loop
         print('Type one of r, p, s, or q.')
 
     # Display what the player chose:
-    if playerMove == 'r':
+    if player_move == 'r':
         print('ROCK versus...')
-    elif playerMove == 'p':
+    elif player_move == 'p':
         print('PAPER versus...')
-    elif playerMove == 's':
+    elif player_move == 's':
         print('SCISSORS versus...')
 
     # Display what the computer chose:
-    randomNumber = random.randint(1, 3)
-    if randomNumber == 1:
-        computerMove = 'r'
+    random_number = random.randint(1, 3)
+    if random_number == 1:
+        computer_move = 'r'
         print('ROCK')
-    elif randomNumber == 2:
-        computerMove = 'p'
+    elif random_number == 2:
+        computer_move = 'p'
         print('PAPER')
-    elif randomNumber == 3:
-        computerMove = 's'
+    elif random_number == 3:
+        computer_move = 's'
         print('SCISSORS')
 
     # Display and record the win/loss/tie:
-    if playerMove == computerMove:
+    if player_move == computer_move:
         print('It is a tie!')
         ties = ties + 1
-    elif playerMove == 'r' and computerMove == 's':
+    elif player_move == 'r' and computer_move == 's':
         print('You win!')
         wins = wins + 1
-    elif playerMove == 'p' and computerMove == 'r':
+    elif player_move == 'p' and computer_move == 'r':
         print('You win!')
         wins = wins + 1
-    elif playerMove == 's' and computerMove == 'p':
+    elif player_move == 's' and computer_move == 'p':
         print('You win!')
         wins = wins + 1
-    elif playerMove == 'r' and computerMove == 'p':
+    elif player_move == 'r' and computer_move == 'p':
         print('You lose!')
         losses = losses + 1
-    elif playerMove == 'p' and computerMove == 's':
+    elif player_move == 'p' and computer_move == 's':
         print('You lose!')
         losses = losses + 1
-    elif playerMove == 's' and computerMove == 'r':
+    elif player_move == 's' and computer_move == 'r':
         print('You lose!')
         losses = losses + 1
