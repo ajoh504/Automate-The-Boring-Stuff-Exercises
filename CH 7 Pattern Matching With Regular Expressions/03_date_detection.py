8633a3ea95edb25a910aec559df2d74cfd81cb9c
Alex <83892813+ajoh504@users.noreply.github.com>
2022-03-23 15:42:54 -0700

    Update 03_date_detection.py
    Edited comments and error codes, 
    Deleted unnecessary comments.

--- CH 7 Pattern Matching With Regular Expressions/03_date_detection.py
+++ CH 7 Pattern Matching With Regular Expressions/03_date_detection.py
@@ -4,9 +4,8 @@
 import re
 
 def regex_date_validator(date: str) -> bool:
-    print('Please enter a date in the following format: dd/mm/yyyy')
-    valid = True # final return value, change to false if date not valid
-    is_leap_year = False # 
+    valid = True # final return value, change to False if date not valid
+    is_leap_year = False # If leap year, change to True
     date_regex = re.compile(r'''
     (\d{2})     # day
     (/)         # separator
@@ -21,17 +20,15 @@ def regex_date_validator(date: str) -> bool:
     month = int(regex_groups[0][2])
     year = int(regex_groups[0][4])
 
-    #print(regex_groups, day, month, year)
-
     # check month and date
     if month > 12 or day > 31:
         valid = False
-        print('Error: Invalid month or date range.')
+        print('Error: invalid month/date range.')
 
     # check 30-day months: April, June, September, November
     if month in (4, 6, 9, 11) and day > 30:
         valid = False
-        print('Error: Invalid date for 30-day months.')
+        print('Error: invalid date. Apr, Jun, Sept, and Nov have 30 days.')
 
     # check leap-year. Must be divisible by 4. If leap year is
     # divisible by 100, must also be divisible by 400
@@ -39,34 +36,30 @@ def regex_date_validator(date: str) -> bool:
         is_leap_year = True
         if year % 100 == 0 and year % 400 == 0:
             is_leap_year = True
-        elif year % 100 == 0 and year % 400 != 0:
+        elif year % 100 == 0 and year % 400 != 0: # if divisible by 100 but not 400, is_leap_year == False
             is_leap_year = False
-        else:
-            is_leap_year = False
-            print('Error: leap year millenium must be divisible by 400')
 
     # check February and compare to leap year
     if month == 2 and is_leap_year == True:
         if day > 29:
             valid = False
-            print('Error: invalid date. Check leap year/day.')
+            print('Error: invalid date. Feb has 29 days during leap years.')
 
     # Check February and compare to non-leap year
     if month == 2 and is_leap_year == False:
         if day > 28:
             valid = False
-            print('Error: Feb has 28 .')
-
+            print('Error: invalid date. Feb has 28 days.')
+    
+    # if all previous conditionals are False, then Date is valid
     if valid == True:
         print('Date is valid')
 
     return valid
 
-print('Please enter a date in the following format: dd/mm/yyyy')
+# loop indefinitely
 while True:
+    print('Please enter a date in the following format: dd/mm/yyyy')
     regex_date_validator(str(input()))
     
-'''if regex_date_validator(str(input())) == False:
-    regex_date_validator(str(input()))
-else:
-    quit();'''
+
