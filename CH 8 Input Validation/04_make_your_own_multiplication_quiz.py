3f470c376202f1b8354dcd384e093d9a0b284187
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-03 07:25:00 -0700

    Update 04_make_your_own_multiplication_quiz.py
    Edited after testing, updated to ask for three tries per question

--- CH 8 Input Validation/04_make_your_own_multiplication_quiz.py
+++ CH 8 Input Validation/04_make_your_own_multiplication_quiz.py
@@ -1,19 +1,38 @@
 #! python3
 # Make your own multiplication quiz
-# INCOMPLETE, needs 8 second timer, and 3 tries per question
+# INCOMPLETE, needs 8 second timer
 import time, random, re
 
 number_of_questions = 10
 correct_answers = 0
-for i in range(1, 10):
-    num1 = random.randint(0,9)
-    num2 = random.randint(0,9)
-    regex = re.compile(r'^\d+$')
-    prompt = 'Question #%s: %s x %s = ' % (i, num1, num2)
-    answer = str(input(prompt))
-    mo = regex.search(answer)
-    if (mo.group() is not None) and (int(mo.group()) == (num1 * num2)):
-        correct_answers += 1
-        print('Correct!')
-    elif mo.group() != (num1 * num2):
-        print('Incorrect!')
+for question_number in range(number_of_questions):
+    # Pick two random numbers
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+    # regex forces answer to be a digit
+    regex = re.compile(r'^\d+$') 
+    prompt = 'Question #%s: %s x %s = ' % (question_number, num1, num2)
+    # 3 tries per question
+    for attempt in range(0, 3): 
+        answer = str(input(prompt))
+        mo = regex.search(answer)
+        if mo == None:
+            print('Incorrect')
+            # if final try is incorrect, print 'out of tries'
+            if attempt == 2:
+                print('Out of tries!')
+            continue
+        elif int(mo.group()) != (num1 * num2):
+            print('Incorrect!')
+            # if final try is incorrect, print 'out of tries'
+            if attempt == 2:
+                print('Out of tries!')
+            continue
+        else:
+            correct_answers += 1
+            print('Correct!')
+            break
+
+time.sleep(1) # Brief pause to let user see the result
+print('Score: %s / %s' % (correct_answers, number_of_questions))
+            
