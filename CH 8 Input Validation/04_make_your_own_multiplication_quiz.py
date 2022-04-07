d1b94b026f928ce7fc058ef4e24da472898a48ad
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-07 15:06:06 -0700

    Update 04_make_your_own_multiplication_quiz.py

--- CH 8 Input Validation/04_make_your_own_multiplication_quiz.py
+++ CH 8 Input Validation/04_make_your_own_multiplication_quiz.py
@@ -3,20 +3,19 @@
 
 import time, random, re, threading
 
-number_of_questions = 10
+NUMBER_OF_QUESTIONS = 10
 correct_answers = 0
 
-# The first for loop determines the number of questions to ask
-for question_number in range(number_of_questions):
+for question_number in range(NUMBER_OF_QUESTIONS):# The first for loop determines the number of questions to ask
     if question_number != 0: # Don't show score on first question
         time.sleep(1) # Brief pause to let user see the result
-        print('Score: %s / %s' % (correct_answers, number_of_questions))
+        print('Score: %s / %s' % (correct_answers, NUMBER_OF_QUESTIONS))
     num1 = random.randint(0, 9) # random number 1
     num2 = random.randint(0, 9) # random number 2
     regex = re.compile(r'^\d+$') #  regex that requires a digit
     prompt = 'Question #%s: %s x %s = ' % (question_number, num1, num2) # prompt is the question to ask the user
-    # The second for loop allows only 3 attempts per question
-    for attempt in range(0, 3):
+    
+    for attempt in range(0, 3): # The second for loop allows only 3 attempts per question
         # create a switch and a timeout function that flips the switch
         timeout_switch = False
         def timeout():
@@ -26,11 +25,11 @@ for question_number in range(number_of_questions):
         eight_seconds.start()  # start timer
         answer = str(input(prompt)) # convert user's answer to string in order to pass it into regex
         mo = regex.search(answer)
+        
         if timeout_switch == True:
             print('Out of time!')
             break
-        # mo == None helps avoid an AttributeError because mo.group()
-        # will throw an error if a non-digit is entered by the user
+        # mo == None helps avoid an AttributeError because mo.group() will throw an error if a non-digit is entered by the user
         elif (mo == None) and (timeout_switch == False):
             eight_seconds.cancel()
             print('Incorrect')
