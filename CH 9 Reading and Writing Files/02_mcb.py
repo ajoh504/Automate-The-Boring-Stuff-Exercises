f94da8ef3b0c207d7cd9fec92d277dc3a9099522
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-17 14:28:00 -0700

    Update 02_mcb.py
    Edited for syntax errors after testing

--- CH 9 Reading and Writing Files/02_mcb.py
+++ CH 9 Reading and Writing Files/02_mcb.py
@@ -16,5 +16,5 @@ elif len(sys.argv) == 2:
     if sys.argv[1].lower() == 'list':
         pyperclip.copy(str(list(mcb_shelf.keys())))
     elif sys.argv[1] in mcb_shelf:
-        pyperclip.copy(mcb_shelf[sys.argb[1]])
+        pyperclip.copy(mcb_shelf[sys.argv[1]])
 mcb_shelf.close()
