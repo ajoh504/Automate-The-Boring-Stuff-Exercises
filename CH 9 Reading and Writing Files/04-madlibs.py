bf3374c7c15b5d3b55a24566f1bd0f87719fdbeb
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-21 04:50:53 -0700

    Update 04-madlibs.py

--- CH 9 Reading and Writing Files/04-madlibs.py
+++ CH 9 Reading and Writing Files/04-madlibs.py
@@ -4,11 +4,24 @@
 
 from pathlib import Path
 
-def madlibs(adj: str, noun1: str, verb: str, noun2: str) -> str:
-
-    MADLIBS_TEMPLATE = f'''
+def madlibs():
+    print('-- Welcome to Mad Libs! ---')
+    print('Please enter an adjective:')
+    adjective = input()
+    print('Please enter a noun:')
+    noun1 = input()
+    print('Please enter a verb:')
+    verb = input()
+    print('Please enter another noun:')
+    noun2 = input()
+    madlibs_text = f'''
     The {adjective} panda walked to the
     {noun1} and then {verb}. A nearby {noun2}
     was unaffected by these events.
     '''
-    p = Path.open('madlibs.txt', 'w')
+    p = open('madlibs.txt', 'w')
+    p.write(madlibs_text)
+    p.close()
+    print(madlibs_text)
+    
+madlibs()
