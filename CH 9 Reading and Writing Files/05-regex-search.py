b4aff61242e19b9388cc3d531752722f7b04c44a
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-23 05:27:39 -0700

    Update 05-regex-search.py

--- CH 9 Reading and Writing Files/05-regex-search.py
+++ CH 9 Reading and Writing Files/05-regex-search.py
@@ -1,19 +1,30 @@
 #! python3
 # 05-regex-search.py -- open all .txt files in a directory to search using a user supplied regex
+# INCOMPLETE: receiving UnicodeDecodeError
 
 import os, re
 from pathlib import Path
 
-# prompt user to change directories
-print('This script allows you to input a regex to search all .txt files in a directory')
-while True:
-    dir = Path(input('Please enter a valid directory path:\n'))
-    if dir.exists() and dir.is_dir():
-        os.chdir(dir)
-        break
-    else:
-        continue
+def main() -> None:
+    # prompt user to change directories
+    print('This script allows you to input a regex to search all .txt files in a directory')
+    while True:
+        dir = Path(input('Please enter a valid directory path:\n'))
+        if dir.exists() and dir.is_dir():
+            os.chdir(dir)
+            break
+        else:
+            continue
 
-# TODO: ask for user supplied regex
+    # ask user for regex and search for .txt files in directory
+    #user_regex = input('Please enter a regex string to search all .txt files:\n')
+    #regex = re.compile(user_regex)
+    cwd = Path.cwd()
+    for windows_path in os.listdir(cwd):
+        if windows_path[-4:] == '.txt':
+            file_object = open(windows_path)
+            read_file = file_object.readlines()
+            print(read_file)
 
-# TODO: search for .txt files in directory
+if __name__ == '__main__':
+    main()
