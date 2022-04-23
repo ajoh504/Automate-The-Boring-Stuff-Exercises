ab64f79a179acd7f3c8329df11b0fc74c1fea9bf
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-23 08:07:19 -0700

    Update 05-regex-search.py

--- CH 9 Reading and Writing Files/05-regex-search.py
+++ CH 9 Reading and Writing Files/05-regex-search.py
@@ -1,6 +1,5 @@
 #! python3
 # 05-regex-search.py -- open all .txt files in a directory to search using a user supplied regex
-# INCOMPLETE: receiving UnicodeDecodeError
 
 import os, re
 from pathlib import Path
@@ -10,21 +9,22 @@ def main() -> None:
     print('This script allows you to input a regex to search all .txt files in a directory')
     while True:
         dir = Path(input('Please enter a valid directory path:\n'))
-        if dir.exists() and dir.is_dir():
+        if dir.is_dir():
             os.chdir(dir)
             break
         else:
             continue
 
     # ask user for regex and search for .txt files in directory
-    #user_regex = input('Please enter a regex string to search all .txt files:\n')
-    #regex = re.compile(user_regex)
+    user_regex = input('Please enter a regex to search all .txt files:\n')
+    regex = re.compile(user_regex)
     cwd = Path.cwd()
     for windows_path in os.listdir(cwd):
         if windows_path[-4:] == '.txt':
-            file_object = open(windows_path)
-            read_file = file_object.readlines()
-            print(read_file)
+            file_object = open(windows_path, encoding='utf8')
+            read_file = file_object.read()
+            print(regex.findall(read_file))
+            file_object.close()
 
 if __name__ == '__main__':
     main()
