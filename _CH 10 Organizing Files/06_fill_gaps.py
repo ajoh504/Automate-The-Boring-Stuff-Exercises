b83b41655766a96219b97a178c6b03224a7153b3
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-02 17:13:11 -0700

    Update 06_fill_gaps.py
    Edited regex to search for numbers, edited for loop to add numbers to list

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -6,25 +6,35 @@
 import re, os, shutil
 
 def fill_gaps(folder: str, prefix: str) -> str:
-
-    # Remove numbers from prefix using a regex
-    remove_num_regex = re.compile('(\D*)')
-    mo = remove_num_regex.search(prefix)
+    # Remove numbers from prefix using a regex, then separate letters and numbers into groups
+    regex = re.compile('(\D*)(\d*)') 
+    mo = regex.search(prefix)
     if mo is None:
-        return 'Invalid prefix. Prefix contains no letters.'
+        return 'Invalid prefix. Prefix contains no letters or numbers.'
     elif os.path.isdir(folder) == False:
         return 'Invalid folder. Argument must be a valid folder path.'
     else:
+        # main program execution
         os.chdir(folder)
-        # Add letter-only prefix to new regex
-        letters_only = mo.group(1)
-        # Search folder for letter prefix and add all matches to a list
-        match_list = [i for i in os.listdir('.') if letters_only in i]
+        letters = mo.group(1)
         
-    # TODO: Rename later files to fill in gaps
-    for index, file_name in enumerate(match_list):
-        if str(index) not in ''.join(match_list):
-            print(folder + '\\' + match_list[-1], folder + '\\' + match_list[index])
-            #shutil.move(folder + '\\' + match_list[-1], folder + '\\' + match_list[index])
+        # Search folder for letters and add matches to a list
+        file_list = [i for i in os.listdir('.') if letters in i]
+        
+        # search match_list for numbers and add them to a list
+        numbers_list = []
+        for file_name in match_list:
+            mo = regex.search(file_name)
+            numbers_list.append(int(mo.group(2)))
 
-fill_gaps('C:\\Users\\Buzzkill\\test_source', 'spam000')
+        # loop through file_list to search for missings numbers
+        for index, file_name in enumerate(file_list, 1):
+            if str(index) not in ''.join(str(numbers_list)):
+                print(index)
+                #print(index, file_list[-1], file_list[index], match_list)
+                print(folder + '\\' + file_list[-1], folder + '\\' + file_list[index])
+                
+                # TODO: Rename later files to fill in gaps
+                #shutil.move(folder + '\\' + file_list[-1], folder + '\\' + file_list[index])
+
+fill_gaps('C:\\test_source', 'spam000')
