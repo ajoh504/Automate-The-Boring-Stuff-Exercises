ccbf896655f39f852e97ded0f9d382e4f70fd767
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-08 06:41:24 -0700

    Update 06_fill_gaps.py
    Refactored to separate code into multiple functions / tested

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -1,64 +1,91 @@
 #! python3
-# 06_fill_gaps.py -- search a single folder, find gaps in numbered files (spam001.txt, spam003.txt)
+# 06_fill_gaps.py -- search a single folder, find gaps in numbered files (ex. spam001.txt, spam003.txt)
 # and rename later files to fill in the gaps.
 
 import re, os, shutil
 from pathlib import Path
 
 
-def fill_gaps(folder: str, prefix: str) -> list | str:
+def return_match_object(prefix: str) -> re.Match:
     # Remove numbers from prefix using a regex, then separate letters and numbers into groups
-    regex = re.compile("(\D*)(\d*)")
-    mo = regex.search(prefix)
-    if mo is None:
+    return re.compile("(\D*)(\d*)").search(prefix)
+
+
+def check_user_input(folder: str, regex_mo: re.Match) -> str | bool:
+    if regex_mo is None:
         return "Invalid prefix. Prefix contains no letters or numbers."
     elif os.path.isdir(folder) == False:
         return "Invalid folder. Argument must be a valid folder path."
     else:
-        # main program execution
-        os.chdir(folder)
-        letters = mo.group(1)
-        # Search folder for letters and add matches to a list
-        file_list = [i for i in os.listdir(".") if letters in i]
+        return False
 
-        # search match_list for numbers and add them to a list
-        numbers_list = []
-        for file_name in file_list:
-            mo = regex.search(file_name)
-            numbers_list.append(int(mo.group(2)))
-            number_prefix_length = len(mo.group(2))
 
-        # loop through file_list to search for missings numbers
-        # Rename later files to fill in gaps
-        return_list = []
-        for index, number in enumerate(numbers_list, 1):
-            if index not in numbers_list:
-                zeroes_to_add = "0" * (number_prefix_length - len(str(index)))
-                suffix = Path(folder + "\\" + file_list[0]).suffix
-                shutil.move(
-                    folder + "\\" + file_list[-1],
-                    folder + "\\" + letters + zeroes_to_add + str(index) + suffix,
-                )
-                return_list.append(
-                    "renamed "
-                    + file_list[-1]
-                    + " to "
-                    + letters
-                    + zeroes_to_add
-                    + str(index)
-                    + suffix
-                )
+def create_file_list(regex_mo: re.Match) -> list:
+    # Search folder for letters and add matches to a list
+    letters = regex_mo.group(1)
+    file_list = [i for i in os.listdir(".") if letters in i]
+    return file_list
 
-        return return_list
+
+def create_number_list(file_list: list, regex_mo: re.Match) -> list:
+    # search match_list for numbers and add them to a list
+    number_list = []
+    for file_name in file_list:
+        mo = return_match_object(file_name)
+        number_list.append(int(mo.group(2)))
+    return number_list
+
+
+def fill_gaps(
+    file_list: list,
+    number_list: list,
+    folder: str,
+    number_prefix_length: int,
+    letters: str,
+) -> list:
+    # Rename later files to fill in gaps
+    return_list = []
+    for index, number in enumerate(number_list, 1):
+        if index not in number_list:
+            zeroes_to_add = "0" * (number_prefix_length - len(str(index)))
+            suffix = Path(folder + "\\" + file_list[0]).suffix
+            shutil.move(
+                folder + "\\" + file_list[-1],
+                folder + "\\" + letters + zeroes_to_add + str(index) + suffix,
+            )
+            return_list.append(
+                "renamed "
+                + file_list[-1]
+                + " to "
+                + letters
+                + zeroes_to_add
+                + str(index)
+                + suffix
+            )
+
+    return return_list
+
+
+def main() -> None:
+    while True:
+        folder = input("Please enter a folder to search for files\n")
+        prefix = input("Please enter a prefix to search for, such as spam000\n")
+        match_object = return_match_object(prefix)
+        letters = match_object.group(1)
+        number_prefix_length = len(match_object.group(2))
+        # check for valid user input
+        if type(check_user_input(folder, match_object)) == str:
+            continue
+        else:
+            # main program execution
+            os.chdir(folder)
+            file_list = create_file_list(match_object)
+            number_list = create_number_list(file_list, match_object)
+            print(
+                fill_gaps(file_list, number_list, folder, number_prefix_length, letters)
+            )
+            break
 
 
 if __name__ == "__main__":
-    print(
-        "This program finds numbered text files, such as spam001 and spam003, \nand renames later files to fill in the numbered gaps"
-    )
-    print(
-        fill_gaps(
-            input("Please enter a folder to search for files\n"),
-            input("Please enter a prefix to search for, such as spam000\n"),
-        )
-    )
+    main()
