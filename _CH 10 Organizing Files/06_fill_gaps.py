f91666d5b5d56fc0b075e8321adedb47a686b803
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-19 05:55:56 -0700

    Update 06_fill_gaps.py
    Edited class method calls

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -12,15 +12,15 @@ class rename_gap_files:
         self.prefix = prefix
 
         
-    def return_match_object(prefix: str) -> re.Match:
+    def return_match_object(self) -> re.Match:
         # Remove numbers from prefix using a regex, then separate letters and numbers into groups
-        return re.compile("(\D*)(\d*)").search(prefix)
+        return re.compile("(\D*)(\d*)").search(self.prefix)
 
 
-    def check_user_input(directory: str, regex_mo: re.Match) -> str | bool:
+    def check_user_input(self, regex_mo: re.Match) -> str | bool:
         if regex_mo is None:
             return "Invalid prefix. Prefix contains no letters or numbers."
-        elif os.path.isdir(directory) == False:
+        elif os.path.isdir(self.directory) == False:
             return "Invalid directory. Argument must be a valid directory path."
         else:
             return False
@@ -43,9 +43,9 @@ class rename_gap_files:
 
 
     def fill_gaps(
+        self,
         file_list: list,
         number_list: list,
-        directory: str,
         number_prefix_length: int,
         letters: str,
     ) -> list:
@@ -54,10 +54,10 @@ class rename_gap_files:
         for index, number in enumerate(number_list, 1):
             if index not in number_list:
                 zeroes_to_add = "0" * (number_prefix_length - len(str(index)))
-                suffix = Path(directory + "\\" + file_list[0]).suffix
+                suffix = Path(self.directory + "\\" + file_list[0]).suffix
                 shutil.move(
-                    directory + "\\" + file_list[-1],
-                    directory + "\\" + letters + zeroes_to_add + str(index) + suffix,
+                    self.directory + "\\" + file_list[-1],
+                    self.directory + "\\" + letters + zeroes_to_add + str(index) + suffix,
                 )
                 return_list.append(
                     "renamed "
@@ -76,20 +76,20 @@ def main() -> None:
     while True:
         directory = input("Please enter a directory to search for files\n")
         prefix = input("Please enter a prefix to search for, such as spam000\n")
-        fill_gaps = rename_gap_files(directory, prefix)
-        match_object = fill_gaps.return_match_object(prefix)
+        #fill_gaps = 
+        match_object = rename_gap_files(directory, prefix).return_match_object()
         letters = match_object.group(1)
         number_prefix_length = len(match_object.group(2))
         # check for valid user input
-        if type(fill_gaps.check_user_input(directory, match_object)) == str:
+        if type(rename_gap_files(directory, prefix).check_user_input(match_object)) == str:
             continue
         else:
             # main program execution
             os.chdir(directory)
-            file_list = fill_gaps.create_file_list(match_object)
-            number_list = fill_gaps.create_number_list(file_list, match_object)
+            file_list = rename_gap_files(directory, prefix).create_file_list(match_object)
+            number_list = rename_gap_files(directory, prefix).create_number_list(file_list, match_object)
             print(
-                fill_gaps.fill_gaps(file_list, number_list, directory, number_prefix_length, letters)
+                rename_gap_files(directory, prefix).fill_gaps(file_list, number_list, number_prefix_length, letters)
             )
             break
 
