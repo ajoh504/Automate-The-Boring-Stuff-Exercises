71f51abe1bfa48db24db9e5c071f66419c396ecd
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-20 05:53:10 -0700

    Update 06_fill_gaps.py
    1. renamed class to correct grammar 2. reformatted class arguments in __init__ function 
    
    TODO: 'number_prefix_length' and 'letters' must be edited -- they are not properly passed into the class

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -1,15 +1,30 @@
 #! python3
 # 06_fill_gaps.py -- search a single folder, find gaps in numbered files (ex. spam001.txt, spam003.txt)
 # and rename later files to fill in the gaps.
+# INCOMPLETE
 
 import re, os, shutil
 from pathlib import Path
 
-class rename_gap_files:
+class rename_gapped_files:
 
-    def __init__(self, directory, prefix) -> None:
+    def __init__(
+        self,
+        directory: str,
+        prefix: str,
+        #regex_mo: re.Match,
+        #file_list: list,
+        #number_list: list,
+        number_prefix_length: int,
+        letters: str,
+    ) -> None:
         self.directory = directory
         self.prefix = prefix
+        #self.regex_mo = regex_mo
+        #self.file_list = file_list
+        #self.number_list = number_list
+        self.number_prefix_length = number_prefix_length
+        self.letters = letters
 
         
     def return_match_object(self) -> re.Match:
@@ -26,44 +41,38 @@ class rename_gap_files:
             return False
 
 
-    def create_file_list(regex_mo: re.Match) -> list:
+    def create_file_list(self, regex_mo: re.Match) -> list:
         # Search directory for letters and add matches to a list
         letters = regex_mo.group(1)
         file_list = [i for i in os.listdir(".") if letters in i]
         return file_list
 
 
-    def create_number_list(file_list: list, regex_mo: re.Match) -> list:
+    def create_number_list(self) -> list:
         # search match_list for numbers and add them to a list
         number_list = []
-        for file_name in file_list:
+        for file_name in self.file_list:
             mo = return_match_object(file_name)
             number_list.append(int(mo.group(2)))
         return number_list
 
 
-    def fill_gaps(
-        self,
-        file_list: list,
-        number_list: list,
-        number_prefix_length: int,
-        letters: str,
-    ) -> list:
+    def fill_gaps(self) -> list:
         # Rename later files to fill in gaps
         return_list = []
-        for index, number in enumerate(number_list, 1):
-            if index not in number_list:
-                zeroes_to_add = "0" * (number_prefix_length - len(str(index)))
+        for index, number in enumerate(self.number_list, 1):
+            if index not in self.number_list:
+                zeroes_to_add = "0" * (self.number_prefix_length - len(str(index)))
                 suffix = Path(self.directory + "\\" + file_list[0]).suffix
                 shutil.move(
-                    self.directory + "\\" + file_list[-1],
-                    self.directory + "\\" + letters + zeroes_to_add + str(index) + suffix,
+                    self.directory + "\\" + self.file_list[-1],
+                    self.directory + "\\" + self.letters + zeroes_to_add + str(index) + suffix,
                 )
                 return_list.append(
                     "renamed "
-                    + file_list[-1]
+                    + self.file_list[-1]
                     + " to "
-                    + letters
+                    + self.letters
                     + zeroes_to_add
                     + str(index)
                     + suffix
@@ -76,20 +85,28 @@ def main() -> None:
     while True:
         directory = input("Please enter a directory to search for files\n")
         prefix = input("Please enter a prefix to search for, such as spam000\n")
-        #fill_gaps = 
-        match_object = rename_gap_files(directory, prefix).return_match_object()
+        rename = rename_gapped_files(directory, prefix)
+        match_object = rename.return_match_object()
         letters = match_object.group(1)
         number_prefix_length = len(match_object.group(2))
         # check for valid user input
-        if type(rename_gap_files(directory, prefix).check_user_input(match_object)) == str:
+        if type(rename.check_user_input(rename.return_match_object())) == str:
             continue
         else:
             # main program execution
             os.chdir(directory)
-            file_list = rename_gap_files(directory, prefix).create_file_list(match_object)
-            number_list = rename_gap_files(directory, prefix).create_number_list(file_list, match_object)
+            file_list = rename.create_file_list(rename.return_match_object())
+            number_list = rename.create_number_list(
+                rename.create_file_list(),
+                rename.return_match_object()
+            )
             print(
-                rename_gap_files(directory, prefix).fill_gaps(file_list, number_list, number_prefix_length, letters)
+                rename.fill_gaps(
+                    rename.create_file_list(),
+                    rename.create_number_list(),
+                    number_prefix_length,
+                    letters
+                )
             )
             break
 
