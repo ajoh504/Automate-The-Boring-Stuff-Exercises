1b072909ef2cac0eab549d6166878f014d8fbadc
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-22 14:09:54 -0700

    Update 06_fill_gaps.py
    Edited to include all TODO items

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -1,23 +1,30 @@
 #! python3
 # 06_fill_gaps.py -- search a single folder, find gaps in numbered files (ex. spam001.txt, spam003.txt)
 # and rename later files to fill in the gaps.
+#
+# (From the book) as an added challenge, insert gaps into numbered files so that
+# new files can be added. See 'class insert_gaps' for solution
+#
+# INCOMPLETE -- see all TODO:
 
-import re, os, shutil
+import re, os, shutil, random
 from pathlib import Path
 
-
+# TODO: remove unnecessary variables -- invoke functions directly instead of in variables?
 class rename_gapped_files:
     def __init__(self, directory: str, prefix: str) -> None:
         self.directory = directory
+        # TODO: rewrite to accept prefix as 'spam' instead of 'spam000'
         self.prefix = prefix
         self.file_list = self.create_file_list(self.return_match_object(self.prefix))
         self.number_list = self.create_number_list()
         self.number_prefix_length = len(self.return_match_object(self.prefix).group(2))
         self.letters = self.return_match_object(self.prefix).group(1)
 
+    # TODO: rewrite code to use glob instead of regex
     def return_match_object(self, prefix) -> re.Match:
         return re.compile("(\D*)(\d*)").search(prefix)
-
+    # TODO: rewrite to include exceptions
     def check_user_input(self, regex_mo: re.Match) -> str | bool:
         if regex_mo is None:
             return "Invalid prefix. Prefix contains no letters or numbers."
@@ -25,13 +32,16 @@ class rename_gapped_files:
             return "Invalid directory. Argument must be a valid directory path."
         else:
             return False
-
+    # TODO: rewrite to use glob instead of os.listdir
+    # TODO: rewrite as generator function
     def create_file_list(self, regex_mo: re.Match) -> list:
         # search current working directory for file names that match regex
         letters = regex_mo.group(1)
-        file_list = [i for i in os.listdir(".") if letters in i]
-        return file_list
-
+        #test_list = [str(file) for file in self.directory.glob(f"{letters}*")]
+        #print(test_list, self.directory, self.prefix)
+        return [i for i in os.listdir(".") if letters in i]
+        #return [str(file) for file in self.directory.glob(f"{letters}*")]
+    # TODO: make functional without mo (match_object)
     def create_number_list(self) -> list:
         # find numbers in matched file names and store numbers in list
         # number_list will be used to find numbered gaps
@@ -40,7 +50,7 @@ class rename_gapped_files:
             mo = self.return_match_object(file_name)
             number_list.append(int(mo.group(2)))
         return number_list
-
+    # TODO: separate gap renaming code from gap identification code
     def fill_gaps(self) -> list:
         # Rename later files to fill in gaps
         return_list = []
@@ -48,6 +58,7 @@ class rename_gapped_files:
             if index not in self.number_list:
                 zeroes_to_add = "0" * (self.number_prefix_length - len(str(index)))
                 suffix = Path(str(self.directory) + "\\" + self.file_list[0]).suffix
+                # TODO: rewrite using Path instead of shutil
                 shutil.move(
                     str(self.directory) + "\\" + self.file_list[-1],
                     str(self.directory)
@@ -68,6 +79,44 @@ class rename_gapped_files:
                 )
 
         return return_list
+# TODO: write added challenge (insert gaps)
+'''
+class insert_gaps:
+    def __init__(self, directory, prefix, rename_gap_class):
+        self.directory = directory
+        self.prefix = prefix
+        self.file_list = rename_gap_class.create_file_list(rename_gap_class.return_match_object(self.prefix))
+        self.letters = rename_gap_class.letters
+        self.number_prefix_length = rename_gap_class.number_prefix_length
+        self.number_list = rename_gap_class.number_list
+
+    def insert_gaps(self):
+        gaps_to_insert = random.randint(1, len(self.number_list))
+        for number in range(gaps_to_insert):
+            zeroes_to_add = "0" * (self.number_prefix_length - len(str(number)))
+            number_to_change = random.choice(self.number_list)
+            new_number = number_to_change + len(self.number_list)
+            new_number_zeroes = "0" * (self.number_prefix_length - len(str(new_number)))
+            suffix = Path(str(self.directory) + "\\" + self.file_list[0]).suffix
+            changed_number_list = []
+            if number_to_change not in changed_number_list:
+                print(
+                    str(self.directory) + '\\' + self.letters + str(zeroes_to_add) + str(number_to_change) + suffix,
+                    str(self.directory) + '\\' + self.letters + str(new_number_zeroes) + str(new_number) + suffix
+                )
+
+                shutil.move(
+                    str(self.directory) + '\\' + self.letters + str(zeroes_to_add) + str(number_to_change) + suffix,
+                    str(self.directory) + '\\' + self.letters + str(new_number_zeroes) + str(new_number) + suffix
+                )
+                changed_number_list.append(number_to_change)
+'''
+'''                
+            for file in self.directory.glob('*'):
+                if self.letters in str(file):
+                    shutil.move(
+                        self.directory + '\\' +
+                    )'''
 
 
 def main() -> None:
@@ -76,12 +125,15 @@ def main() -> None:
         prefix = (input("Please enter a prefix to search for, such as spam000\n"))
         os.chdir(directory)
         rename = rename_gapped_files(directory, prefix)
+        #insert = insert_gaps(directory, prefix, rename)
         match_object = rename.return_match_object(prefix)
 
         if type(rename.check_user_input(match_object)) == str:
             continue
         else:
+
             print(rename.fill_gaps())
+            #insert.insert_gaps()
             break
 
 
