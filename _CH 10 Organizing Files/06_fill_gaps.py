82ce091978989628a30e382cc8509cb18e230dc6
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-24 18:25:38 -0700

    Update 06_fill_gaps.py
    1. removed check_user_input 
    2. rewrote create_file_list() and create_number_list() to be usable without match object (regex)
    3. rewrote create_file_list() to be usable without os module / removed os module entirely
    4. rewrote code to work without regex / removed re module
    5. rewrote to accept prefix as 'spam' instead of 'spam000'

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -7,49 +7,29 @@
 #
 # INCOMPLETE -- see all TODO:
 
-import re, os, shutil, random
+import shutil, random
 from pathlib import Path
 
 # TODO: remove unnecessary variables -- invoke functions directly instead of in variables?
 class rename_gapped_files:
     def __init__(self, directory: str, prefix: str) -> None:
         self.directory = directory
-        # TODO: rewrite to accept prefix as 'spam' instead of 'spam000'
         self.prefix = prefix
-        self.file_list = self.create_file_list(self.return_match_object(self.prefix))
+        self.file_list = self.create_file_list()
         self.number_list = self.create_number_list()
-        self.number_prefix_length = len(self.return_match_object(self.prefix).group(2))
-        self.letters = self.return_match_object(self.prefix).group(1)
+        self.number_prefix_length = len(Path(self.file_list[0]).stem.strip(self.prefix))
 
-    # TODO: rewrite code to use glob instead of regex
-    def return_match_object(self, prefix) -> re.Match:
-        return re.compile("(\D*)(\d*)").search(prefix)
-    # TODO: rewrite to include exceptions
-    def check_user_input(self, regex_mo: re.Match) -> str | bool:
-        if regex_mo is None:
-            return "Invalid prefix. Prefix contains no letters or numbers."
-        elif os.path.isdir(self.directory) == False:
-            return "Invalid directory. Argument must be a valid directory path."
-        else:
-            return False
-    # TODO: rewrite to use glob instead of os.listdir
     # TODO: rewrite as generator function
-    def create_file_list(self, regex_mo: re.Match) -> list:
-        # search current working directory for file names that match regex
-        letters = regex_mo.group(1)
-        #test_list = [str(file) for file in self.directory.glob(f"{letters}*")]
-        #print(test_list, self.directory, self.prefix)
-        return [i for i in os.listdir(".") if letters in i]
-        #return [str(file) for file in self.directory.glob(f"{letters}*")]
-    # TODO: make functional without mo (match_object)
+    def create_file_list(self) -> list:
+        return [str(file) for file in self.directory.glob(f"{self.prefix}*")]
+
     def create_number_list(self) -> list:
         # find numbers in matched file names and store numbers in list
-        # number_list will be used to find numbered gaps
         number_list = []
         for file_name in self.file_list:
-            mo = self.return_match_object(file_name)
-            number_list.append(int(mo.group(2)))
+            number_list.append(int(str(Path(file_name).stem).strip(self.prefix)))
         return number_list
+
     # TODO: separate gap renaming code from gap identification code
     def fill_gaps(self) -> list:
         # Rename later files to fill in gaps
@@ -60,10 +40,10 @@ class rename_gapped_files:
                 suffix = Path(str(self.directory) + "\\" + self.file_list[0]).suffix
                 # TODO: rewrite using Path instead of shutil
                 shutil.move(
-                    str(self.directory) + "\\" + self.file_list[-1],
+                    str(self.file_list[-1]),
                     str(self.directory)
                     + "\\"
-                    + self.letters
+                    + self.prefix
                     + zeroes_to_add
                     + str(index)
                     + suffix,
@@ -72,7 +52,7 @@ class rename_gapped_files:
                     "renamed "
                     + self.file_list[-1]
                     + " to "
-                    + self.letters
+                    + self.prefix
                     + zeroes_to_add
                     + str(index)
                     + suffix
@@ -120,21 +100,13 @@ class insert_gaps:
 
 
 def main() -> None:
-    while True:
-        directory = Path(input("Please enter a directory to search for files\n"))
-        prefix = (input("Please enter a prefix to search for, such as spam000\n"))
-        os.chdir(directory)
-        rename = rename_gapped_files(directory, prefix)
-        #insert = insert_gaps(directory, prefix, rename)
-        match_object = rename.return_match_object(prefix)
+    directory = Path(input("Please enter a directory to search for files\n"))
+    prefix = input("Please enter a prefix to search for, such as spam\n")
+    rename = rename_gapped_files(directory, prefix)
+    #insert = insert_gaps(directory, prefix, rename)
+    print(rename.fill_gaps())
+    #insert.insert_gaps()
 
-        if type(rename.check_user_input(match_object)) == str:
-            continue
-        else:
-
-            print(rename.fill_gaps())
-            #insert.insert_gaps()
-            break
 
 
 if __name__ == "__main__":
