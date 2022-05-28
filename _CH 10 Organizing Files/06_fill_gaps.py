fbd8eaf29be0f945d3bd0b6a0f9a49d7c24eb0b6
Alex <83892813+ajoh504@users.noreply.github.com>
2022-05-28 07:52:43 -0700

    Update 06_fill_gaps.py
    1. separated gap identification code from renaming code
    2. rewrote function names for clarity
    3. corrected logic error in main() while loop
    4. rewrote code to use Path instead of Path and shutil

--- _CH 10 Organizing Files/06_fill_gaps.py
+++ _CH 10 Organizing Files/06_fill_gaps.py
@@ -7,106 +7,96 @@
 #
 # INCOMPLETE -- see all TODO:
 
-import shutil, random
+
+import random
 from pathlib import Path
 
 
-class rename_gapped_files:
-    def __init__(self, directory: str, prefix: str) -> None:
+class renameFiles:
+    def __init__(self, directory: Path, prefix: str) -> None:
         self.directory = directory
         self.prefix = prefix
         self.number_prefix_length = len(
-            Path(self.create_file_list()[0]).stem.strip(self.prefix)
+            Path(self.return_file_list()[0]).stem.strip(self.prefix)
         )
 
-    # TODO: rewrite as generator function
-    def create_file_list(self) -> list:
+    def return_file_list(self) -> list:
         return [str(file) for file in self.directory.glob(f"{self.prefix}*")]
 
-    def create_number_list(self) -> list:
+    def return_number_list(self) -> list:
         # find numbers in matched file names and store numbers in list
+        file_list = self.return_file_list()
         number_list = []
-        for file_name in self.create_file_list():
+        for file_name in file_list:
             number_list.append(int(str(Path(file_name).stem).strip(self.prefix)))
         return number_list
 
-    def zeroes_to_add(self, number):
+    def zeroes_to_add(self, number) -> str:
         return "0" * (self.number_prefix_length - len(str(number)))
 
-    def return_suffix(self):
-        return Path(str(self.directory) + "\\" + self.create_file_list()[0]).suffix
+    def return_suffix(self) -> str:
+        return Path(str(self.directory) + "\\" + self.return_file_list()[0]).suffix
 
-    # TODO: separate gap renaming code from gap identification code
-    def fill_gaps(self) -> None:
-        # Rename later files to fill in gaps
-        for index, number in enumerate(self.create_number_list(), 1):
-            if index not in self.create_number_list():
-                # TODO: rewrite using Path instead of shutil
-                shutil.move(
-                    str(self.create_file_list()[-1]),
+    def return_gap_list(self) -> list:
+        number_list = self.return_number_list()
+        gap_list = []
+        for gap in range(len(number_list)):
+            if gap not in number_list:
+                gap_list.append(gap)
+        return gap_list
+
+    def fill_gaps(self):
+        gap_list = self.return_gap_list()
+        for gap in gap_list:
+            Path.rename(
+                Path(self.return_file_list()[-1]),
+                Path(
                     str(self.directory)
                     + "\\"
                     + self.prefix
-                    + self.zeroes_to_add(index)
-                    + str(index)
-                    + self.return_suffix(),
+                    + self.zeroes_to_add(gap)
+                    + str(gap) + self.return_suffix()
                 )
+            )
 
-    # TODO: write added challenge (insert gaps)
-    def insert_gaps(self):
-        gaps_to_insert = random.randint(1, len(self.create_number_list()))
-        for number in range(gaps_to_insert):
-            number_to_change = random.choice(self.create_number_list())
-            new_number = number_to_change + len(self.create_number_list())
-            changed_number_list = []
-            if number_to_change not in changed_number_list:
-                print(
-                    str(self.directory)
-                    + "\\"
-                    + self.prefix
-                    + str(self.zeroes_to_add(number))
-                    + str(number_to_change)
-                    + self.return_suffix(),
-                    str(self.directory)
-                    + "\\"
-                    + self.prefix
-                    + str(self.zeroes_to_add(new_number))
-                    + str(new_number)
-                    + self.return_suffix(),
-                )
-
-                shutil.move(
-                    str(self.directory)
-                    + "\\"
-                    + self.prefix
-                    + str(self.zeroes_to_add(number))
-                    + str(number_to_change)
-                    + self.return_suffix(),
-                    str(self.directory)
-                    + "\\"
-                    + self.prefix
-                    + str(self.zeroes_to_add(new_number))
-                    + str(new_number)
-                    + self.return_suffix(),
-                )
-                changed_number_list.append(number_to_change)
-
+# TODO: write added challenge (insert gaps)
+#     def insert_gaps(self):
+#         gaps_to_insert = random.randint(1, len(self.return_number_list()))
+#         for number in range(gaps_to_insert):
+#             number_to_change = random.choice(self.return_number_list())
+#             new_number = number_to_change + len(self.return_number_list())
+#             changed_number_list = []
+#             if number_to_change not in changed_number_list:
+#                 shutil.move(
+#                     str(self.directory)
+#                     + "\\"
+#                     + self.prefix
+#                     + str(self.zeroes_to_add(number))
+#                     + str(number_to_change)
+#                     + self.return_suffix,
+#                     str(self.directory)
+#                     + "\\"
+#                     + self.prefix
+#                     + str(self.zeroes_to_add(new_number))
+#                     + str(new_number)
+#                     + self.return_suffix,
+#                 )
+#                 changed_number_list.append(number_to_change)
 
 
 def main() -> None:
+    directory = Path(input("Please enter a directory to search for files\n"))
+    prefix = input("Please enter a prefix to search for, such as spam\n")
     while True:
-        directory = Path(input("Please enter a directory to search for files\n"))
-        prefix = input("Please enter a prefix to search for, such as spam\n")
-        choice = input("Type '1' to rename gapped files, or '2' to to insert gaps")
-        rename = rename_gapped_files(directory, prefix)
+        choice = input("Type '1' to rename files, or '2' to to insert gaps")
+        rename = renameFiles(directory, prefix)
         match choice:
             case '1':
                 rename.fill_gaps()
                 break
             case '2':
-                rename.insert_gaps()
+                #rename.insert_gaps()
                 break
-
 
 
 if __name__ == "__main__":
