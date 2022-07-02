340f35ebcde03a450fb969fd3bf01c36e281eeeb
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-02 15:44:43 -0700

    Update 06_2048.py
    1. removed grid

--- _CH 12 Web Scraping/06_2048.py
+++ _CH 12 Web Scraping/06_2048.py
@@ -3,28 +3,45 @@
 # game url: https://play2048.co/
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import random
+import time
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 logging.info('Program start')
 
 class gameBot2048:
     def __init__(game):
-        game.grid_state = {
-            '1x1': '', '1x2': '', '1x3': '', '1x4': '',
-            '2x1': '', '2x2': '', '2x3': '', '2x4': '',
-            '3x1': '', '3x2': '', '3x3': '', '3x4': '',
-            '4x1': '', '4x2': '', '4x3': '', '4x4': ''
-        }
+        game.browser = webdriver.Firefox()
+        game.moves = (
+            Keys.ARROW_UP,
+            Keys.ARROW_DOWN,
+            Keys.ARROW_LEFT,
+            Keys.ARROW_RIGHT
+        )
 
-    def get_current_grid(game):
-        pass
-    
-    def take_next_move(game):
-        pass
+
+    def play_game(game):
+        game.browser.get('https://play2048.co/')
+        while True:
+            time.sleep(1)
+            x = random.randint(0, 3)
+            game.browser.find_element(By.CSS_SELECTOR, 'html').send_keys(game.moves[x])
+
 
 def main():
     current_game = gameBot2048()
+    current_game.play_game()
 
 if __name__ == "__main__":
     main()
+
+'''
+<div class="game-message game-over"><p>Game over!</p><div class="lower"><a class="keep-playing-button">Keep going</a>
+<a class="retry-button">Try again</a></div></div>
+
+'''
+
+
