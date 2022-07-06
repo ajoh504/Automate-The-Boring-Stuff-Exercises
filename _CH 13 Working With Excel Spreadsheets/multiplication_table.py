d65afba888c6de00798a026a8bc3d96c1b611679
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-06 16:22:01 -0700

    Update multiplication_table.py

--- _CH 13 Working With Excel Spreadsheets/multiplication_table.py
+++ _CH 13 Working With Excel Spreadsheets/multiplication_table.py
@@ -3,17 +3,19 @@
 # multiplication table in Excel with the contents N x N. Make the first row and
 # column bold. sys.argv[1] = N
 
-import openpyxl
 import sys
+import openpyxl
+from openpyxl.styles import Font
 
 def create_multiplication_table():
     wb = openpyxl.Workbook() # create Workbook object
     sheet = wb.active # create Workbook sheet object
-    # todo: create bold font type
+    bold = Font(bold=True)
 
     #todo: loop through rows and columns, and append N to cells
-    for i in range(1, sys.argv[1]):
-        print(sheet[i])
+    for row_num in range(1, int(sys.argv[1])):
+        for column_num in range(0, int(sys.argv[1])):
+            print(sheet[row_num][column_num])
 
 if __name__ == '__main__':
     create_multiplication_table()
