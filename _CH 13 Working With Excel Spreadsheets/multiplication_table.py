664559f268cbd9d63235ca0099de09f34de07fcf
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-07 15:45:58 -0700

    Update multiplication_table.py

--- _CH 13 Working With Excel Spreadsheets/multiplication_table.py
+++ _CH 13 Working With Excel Spreadsheets/multiplication_table.py
@@ -1,21 +1,22 @@
 #!python3
-# multiplication_table.py -- take a number N from the command line and create a
-# multiplication table in Excel with the contents N x N. Make the first row and
-# column bold. sys.argv[1] = N
+# multiplication_table.py -- take a number (num) from the command line and create a
+# multiplication table in Excel with the contents num x num. Make the first row and
+# column bold.
 
 import sys
 import openpyxl
 from openpyxl.styles import Font
 
 def create_multiplication_table():
+    num = int(sys.argv[1])
     wb = openpyxl.Workbook() # create Workbook object
     sheet = wb.active # create Workbook sheet object
     bold = Font(bold=True)
 
     #todo: loop through rows and columns, and append N to cells
-    for row_num in range(1, int(sys.argv[1])):
-        for column_num in range(0, int(sys.argv[1])):
-            print(sheet[row_num][column_num])
+    for row_num in range(2, num + 2):
+        sheet.cell(row=row_num, column=1).value = row_num - 1
+    wb.save('mult_table_' + str(num) + 'x' + str(num) + '.xlsx')
 
 if __name__ == '__main__':
     create_multiplication_table()
