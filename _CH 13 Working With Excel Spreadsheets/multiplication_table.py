d4cd05c2610574dc2def92a92c7f5d817be4d8f9
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-07 16:19:13 -0700

    Update multiplication_table.py
    1. created loop to loop through rows and columns
    
    2. loop makes the first row bold and the first column bold
    
    3. then, loop creates the multiplication table.

--- _CH 13 Working With Excel Spreadsheets/multiplication_table.py
+++ _CH 13 Working With Excel Spreadsheets/multiplication_table.py
@@ -7,16 +7,30 @@ import sys
 import openpyxl
 from openpyxl.styles import Font
 
+
 def create_multiplication_table():
     num = int(sys.argv[1])
-    wb = openpyxl.Workbook() # create Workbook object
-    sheet = wb.active # create Workbook sheet object
+    wb = openpyxl.Workbook()  # create Workbook object
+    sheet = wb.active  # create Workbook sheet object
     bold = Font(bold=True)
 
-    #todo: loop through rows and columns, and append N to cells
-    for row_num in range(2, num + 2):
-        sheet.cell(row=row_num, column=1).value = row_num - 1
-    wb.save('mult_table_' + str(num) + 'x' + str(num) + '.xlsx')
+    for row_num in range(1, num + 2):
+        for column_number in range(1, num + 2):
+            # make first column with bold font
+            if row_num == 1 and column_number > 1:
+                sheet.cell(row=row_num, column=column_number).value = column_number - 1
+                sheet.cell(row=row_num, column=column_number).font = bold
+            # make first row with bold font
+            elif column_number == 1 and row_num > 1:
+                sheet.cell(row=row_num, column=column_number).value = row_num - 1
+                sheet.cell(row=row_num, column=column_number).font = bold
+            # create multiplication table
+            elif column_number > 1 and row_num > 1:
+                sheet.cell(row=row_num, column=column_number).value = (row_num - 1) * (
+                    column_number - 1
+                )
+    wb.save("mult_table_" + str(num) + "x" + str(num) + ".xlsx")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_multiplication_table()
