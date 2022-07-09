dfb803cd00db31d189ea90078df7c64fd3cbe16c
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-08 18:04:04 -0700

    Update blank_row_inserter.py
    1. added docstring
    
    2. shortened variables for readability

--- _CH 13 Working With Excel Spreadsheets/blank_row_inserter.py
+++ _CH 13 Working With Excel Spreadsheets/blank_row_inserter.py
@@ -1,26 +1,52 @@
 #!python3
 # blank_row_inserter.py - take two numbers and an Excel filename as command line
-# arguments. The first number denotes a row number. The second number denotes
-# how many blank rows to place starting at the given row number.
+#                         arguments. The first number denotes a row number. The second
+#                         number denotes how many blank rows to place starting at the
+#                         given row number.
 
 import sys
 import openpyxl
 from openpyxl.utils import get_column_letter, column_index_from_string
 
-def blank_row_inserter(blank_row_starting_point: int, blank_row_quantity: int, excel_file: str) -> None:
+
+def blank_row_inserter(start: int, qty: int, excel_file: str) -> None:
+    """
+    :param start: blank row starting point (sys.argv[1])
+    :param qty: quantity of blank rows to add (sys.argv[2])
+
+    Use nested loops to append Excel cell coordinates to dictionary keys. Use the same coordinates
+    to append the cell values to the dictionary values. If the iterator == the blank row starting
+    point (or) if the iterator < (blank row starting point + blank row qty), then add None as
+    values to create blank lines. Loop continues after blank lines, and appends all the rest of the
+    Excel values.
+    """
     wb = openpyxl.load_workbook(excel_file)
     sheet = wb.active
     new_cells = {}
+    for row in range(
+        1, sheet.max_row + qty + 1
+    ):  # add 1 to account for non-zero starting index
+        for column in range(1, sheet.max_column + 1):
+            if row < start:
+                new_cells[get_column_letter(column) + str(row)] = sheet[
+                    get_column_letter(column) + str(row)
+                ].value
+            elif row == start or row < (start + qty):
+                new_cells[get_column_letter(column) + str(row)] = None
+            elif row >= start + qty:
+                new_cells[get_column_letter(column) + str(row)] = sheet[
+                    get_column_letter(column) + str(row - qty)
+                ].value
 
-    # todo: store contents to copy into dict
-    for row_num in range(1, sheet.max_row):
-        for column_num in range(1, sheet.max_column):
-            pass
-            # if row_num == blank_row_starting_point:
-            #     new_cells[get_column_letter(column_num) + str(row_num)] = sheet.cell(row=row_num, column=column_num)
+    # append dictionary values to Excel cells, then save the new file
+    for row in range(1, sheet.max_row + qty + 1):
+        for column in range(1, sheet.max_column + 1):
+            sheet[get_column_letter(column) + str(row)].value = new_cells[
+                get_column_letter(column) + str(row)
+            ]
 
-    # todo: overwrite cells using dictionary contents
-    wb.save(excel_file[0:-5] + 'RowsInserted' + '.xlsx') # save as new file
+    wb.save(excel_file[0:-5] + "_edited" + ".xlsx")
+
 
 if __name__ == "__main__":
-    blank_row_inserter(sys.argv[1], sys.argv[2], sys.argv[3])
+    blank_row_inserter(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
