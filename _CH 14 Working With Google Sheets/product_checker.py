3c7e07f3e0867138b434afc5d288e0bc8b41f9d9
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-15 18:45:10 -0700

    Create product_checker.py

---
+++ _CH 14 Working With Google Sheets/product_checker.py
@@ -0,0 +1,29 @@
+#!python3
+# sum_checker.py - Open the given spreadsheet, and check the product of the first two columns
+#                  for any mistakes.
+
+
+import ezsheets
+
+
+def product_checker():
+    ss = ezsheets.Spreadsheet("1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg")
+    try:
+        for row_num in range(1, len(ss[0].getRows())):
+            if int(ss[0].getRow(row_num + 1)[0]) * int(
+                ss[0].getRow(row_num + 1)[1]
+            ) == int(ss[0].getRow(row_num + 1)[2]):
+                continue
+            elif not int(ss[0].getRow(row_num + 1)[0]) * int(
+                ss[0].getRow(row_num + 1)[1]
+            ) == int(ss[0].getRow(row_num + 1)[2]):
+                print(
+                    "Row number "
+                    + str(row_num)
+                    + " returned an error. Check your math!"
+                )
+    except ValueError:
+        pass
+
+
+product_checker()
