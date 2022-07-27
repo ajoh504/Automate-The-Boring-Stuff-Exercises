94535879c4dd50e9607faab11706fc49fb5b3dee
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-26 17:46:40 -0700

    Update comicdownloader.py
    1. edited ScriptConfig functions

--- _CH 17 Keeping Time, Scheduling Tasks, And Launching Programs/comicdownloader.py
+++ _CH 17 Keeping Time, Scheduling Tasks, And Launching Programs/comicdownloader.py
@@ -16,38 +16,37 @@ import bs4
 import requests
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG, format=" %(asctime)s -  %(levelname)s -  %(message)s"
-)
-logging.info("Program start")
-
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
+logging.info('Program start')
 
 class ScriptConfig:
     def __init__(self):
         pass
 
-    def schedule_task(self):
-        """Check to see if scheduled task exists. If not, create it."""
-        pass
-
-    def make_batch_file(self):
+    def make_bat_file(self):
         """
-        Scheduled task runs off the main batch file. Check if batch file exists
+        Scheduled task runs from the file download_xkcd.bat. Check if .bat file exists
         in the current working directory. If not, create it.
         """
-        pass
+        if not os.path.isfile(f'{os.getcwd()}\\download_xkcd.bat'):
+            with open('download_xkcd.bat', 'w') as bat_file:
+                bat_file.write(f'py.exe {os.getcwd()}\\comicdownloader.py')
 
+    def schedule_task(self):
+        """Check to see if scheduled task exists. If not, create it."""
+        os.system(f'SCHTASKS /Create /SC DAILY /ST 20:00 /TN "Download XKCD" /TR "{os.getcwd()}\\download_xkcd.bat"')
+        # SCHTASKS /Create /SC DAILY /ST 20:00 /TN "Download XKCD" /TR "CWD\download_xkcd.bat"
 
 class XkcdDownloader:
-    def __init__(self):
-        self.comic_num = 46  # todo: number for testing. Remove in final script
+    def __init__(self, comic_num: int):
+        self.comic_num = comic_num
 
     def get_soup_object(self) -> bs4.BeautifulSoup:
         """Given a comic number, download and parse its html contents from xkcd.com."""
-        print(f"Downloading page https://xkcd.com/{self.comic_num}...")
-        res = requests.get(f"https://xkcd.com/{self.comic_num}")
+        print(f'Downloading page https://xkcd.com/{self.comic_num}...')
+        res = requests.get(f'https://xkcd.com/{self.comic_num}')
         res.raise_for_status()
-        return bs4.BeautifulSoup(res.text, "lxml")
+        return bs4.BeautifulSoup(res.text, 'lxml')
 
     def download_image(self) -> tuple:
         """
@@ -60,9 +59,9 @@ class XkcdDownloader:
         if comic_elem == []:
             pass
         else:
-            image_url = comic_elem[0].get("src")
-            print(f"Downloading image from {image_url}")
-            res = requests.get(f"https:{image_url}")
+            image_url =  comic_elem[0].get('src')
+            print(f'Downloading image from {image_url}')
+            res = requests.get(f'https:{image_url}')
             res.raise_for_status()
             return res, image_url
 
@@ -73,16 +72,16 @@ class XkcdDownloader:
         """
         res = self.download_image()[0]
         image_url = self.download_image()[1]
-        image_file = open(os.path.join("xkcd", os.path.basename(image_url)), "wb")
+        image_file = open(os.path.join('xkcd', os.path.basename(image_url)), 'wb')
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
 
-
 def main():
-    os.makedirs("xkcd", exist_ok=True)  # store comics in ./xkcd
-    XkcdDownloader().save_image()
-
+    os.makedirs('xkcd', exist_ok=True)  # store comics in ./xkcd
+    ScriptConfig().make_bat_file() # First checks if .bat file exists
+    ScriptConfig().schedule_task() # First checks if task exists
+    # XkcdDownloader(46).download_image()
 
 if __name__ == "__main__":
     main()
