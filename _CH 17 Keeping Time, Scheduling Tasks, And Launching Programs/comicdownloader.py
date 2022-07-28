7450f4711f04864a7b773abdc4687f81a741659a
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-07-27 17:40:15 -0700

    Update comicdownloader.py
    1. Edited docstrings for clarity

--- _CH 17 Keeping Time, Scheduling Tasks, And Launching Programs/comicdownloader.py
+++ _CH 17 Keeping Time, Scheduling Tasks, And Launching Programs/comicdownloader.py
@@ -4,8 +4,6 @@
 #                      comic is not in the download directory, then download it.
 
 # todo: write main() logic flow
-# todo: check if comics have been downloaded previously?
-# todo: edit docstrings for clarity
 
 
 import os
@@ -24,22 +22,79 @@ logging.info("Program start")
 
 class XkcdDownloader:
     def __init__(self, comic_num: int):
-        self.comic_num = comic_num
+        self.comic_num = self.get_recent_comic_num
 
-    def make_bat_file(self):
+    def get_soup_object(self) -> bs4.BeautifulSoup:
         """
-        Scheduled task runs from the file download_xkcd.bat. Check if .bat file exists
-        in the current working directory. If not, create it.
+        The urls for XKCD comics contain numbers. Append the comic number to the end of the
+        following url: https://xkcd.com/. Then, download and parse the html contents from
+        the image webpage at xkcd.com.
+
+        :return: A beautiful soup object containing the html contents of the image webpage.
+        """
+        print(f"Downloading page https://xkcd.com/{self.comic_num}...")
+        res = requests.get(f"https://xkcd.com/{self.comic_num}")
+        logging.info("res.text = " + res.text)
+        res.raise_for_status()
+        return bs4.BeautifulSoup(res.text, "lxml")
+
+    def download_image(self) -> tuple:
+        """
+        Call get_soup_object() to get the Bs4 object. Use the select() method on the Bs4
+        object to retrieve the html image contents and pass it to the variable image_elem.
+        Retrieve the image url from image_elem, then download the image into a requests
+        object.
+
+        :return tuple: Index 0 of the tuple contains the reqests object. Index 1 contains the
+            image url.
+        """
+        image_elem = self.get_soup_object().select("#comic img")
+        # < a
+        # rel = "prev"
+        # href = "/2649/"
+        # accesskey = "p" > & lt;
+        # Prev < / a >
+        if image_elem == []:
+            pass
+        else:
+            image_url = image_elem[0].get("src")
+            print(f"Downloading image from {image_url}")
+            res = requests.get(f"https:{image_url}")
+            res.raise_for_status()
+            return res, image_url
+
+    def save_image(self) -> None:
+        """
+        Get the requests object and the image_url from self.download_images(). Open a file
+        with the image_url basename. Add the comic number to the beginning of the file name,
+        save the file, then close it.
+        """
+        res = self.download_image()[0]  # requests object
+        image_url = self.download_image()[1]  # image url
+        image_file = open(
+            os.path.join("xkcd", f"{self.comic_num}_{os.path.basename(image_url)}"),
+            "wb",
+        )
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+
+    @staticmethod
+    def make_bat_file():
+        """
+        Scheduled task runs from the following file: download_xkcd.bat. Check if download_xkcd.bat
+        exists in the current working directory. If it does not exist, create it.
         """
         if not os.path.isfile(f"{os.getcwd()}\\download_xkcd.bat"):
             with open("download_xkcd.bat", "w") as bat_file:
                 bat_file.write(f"py.exe {os.getcwd()}\\comicdownloader.py")
 
-    def schedule_task(self):
+    @staticmethod
+    def schedule_task():
         """
-        In the try statement, run subprocess.check_output() to see if the scheduled
-        task exists. If it does not exist, create the scheduled task inside the
-        except clause.
+        Try subprocess.check_output() to see if the scheduled task exists. If it does not exist,
+        the CalledProcessError exception will be thrown. In which case, create the scheduled task
+        inside the except clause.
         """
         try:
             subprocess.check_output('SCHTASKS /Query /TN "Download XKCD"')
@@ -48,48 +103,12 @@ class XkcdDownloader:
                 f'SCHTASKS /Create /SC DAILY /ST 20:00 /TN "Download XKCD" /TR "{os.getcwd()}\\download_xkcd.bat"'
             )
 
-    def get_soup_object(self) -> bs4.BeautifulSoup:
-        """Given a comic number, download and parse its html contents from xkcd.com."""
-        print(f"Downloading page https://xkcd.com/{self.comic_num}...")
-        res = requests.get(f"https://xkcd.com/{self.comic_num}")
-        res.raise_for_status()
-        return bs4.BeautifulSoup(res.text, "lxml")
-
-    def download_image(self) -> tuple:
-        """
-        Get the image url from the comic element tag, then download the image.
-
-        :return tuple: Index 0 contains the reqests object. Index 1 contains the
-            image url.
-        """
-        comic_elem = self.get_soup_object().select("#comic img")
-        if comic_elem == []:
-            pass
-        else:
-            image_url = comic_elem[0].get("src")
-            print(f"Downloading image from {image_url}")
-            res = requests.get(f"https:{image_url}")
-            res.raise_for_status()
-            return res, image_url
-
-    def save_image(self) -> None:
-        """
-        Get the requests object and the image_url from self.download_images(). Open
-        a file with the image_url basename, save the file, then close it.
-        """
-        res = self.download_image()[0]
-        image_url = self.download_image()[1]
-        image_file = open(os.path.join("xkcd", os.path.basename(image_url)), "wb")
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
-
 
 def main():
     os.makedirs("xkcd", exist_ok=True)  # store comics in ./xkcd
-    XkcdDownloader().make_bat_file()  # First checks if .bat file exists
-    XkcdDownloader().schedule_task()  # First checks if task exists
-    # XkcdDownloader(46).download_image()
+    XkcdDownloader.make_bat_file()  # First checks if .bat file exists
+    XkcdDownloader.schedule_task()  # First checks if task exists
+    XkcdDownloader(46).save_image()
 
 
 if __name__ == "__main__":
